f52cde7bc19b681f4848688a118927a3bd1b99dc
vineethjkatti-tech <vineethjkatti@gmail.com>
2026-03-07 19:26:16 +0530

    Delete src directory

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://kdqhjcncsskyxtfhnkev.supabase.co"
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkcWhqY25jc3NreXh0Zmhua2V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyOTA1MTgsImV4cCI6MjA4Nzg2NjUxOH0.gy8KX-gAvIWXt0I-LQsU27J1dKycH_NKQrj_LHjdS18"
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
